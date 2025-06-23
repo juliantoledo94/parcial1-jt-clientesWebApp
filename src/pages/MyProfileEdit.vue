7fe8c4d5f85af8ce681b1d995ac0ca80390f1e2f
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2025-06-23 18:46:02 -0300

    feat migrate myProfileEdit to composition Api

--- src/pages/MyProfileEdit.vue
+++ src/pages/MyProfileEdit.vue
@@ -1,11 +1,76 @@
-<script>
+<script setup>
+import { onMounted, ref } from 'vue';
 import MainButton from '../components/MainButton.vue';
 import MainH1 from '../components/MainH1.vue';
 import MainLoader from '../components/MainLoader.vue';
-import { subscribeToUserState, updateAuthUserProfile } from '../services/auth';
+import useAuthUserState from '../composables/useAuthUserState';
+import {  updateAuthUserProfile } from '../services/auth';
+
+const { user } = useAuthUserState();
+
+const { profile, updating, feedback, handleSubmit } = useProfileEditForm(user)
+
+function useProfileEditForm(user) {
+    const profile = ref({
+        display_name: null,
+        bio: null,
+        career: null,
+
+    });
+
+    const updating = ref(false);
+    const feedback = ref({
+        type: 'success',
+        message: null,
+    });
+
+    async function handleSubmit() {
+        feedback.message = null;
+
+        try {
+            if (updating.value) return;
+
+            updating.value = true;
+            await updateAuthUserProfile({
+                ...profile.value,
+            });
+
+            feedback.value = {
+                type: 'success',
+                message: 'Tu perfil se actualizó con éxito.',
+
+            }
+
+        } catch (error) {
+            feedback.value = {
+                type: 'error',
+                message: 'Ocurrió un error al actualizar el perfil.',
+            }
+        }
+        updating.value = false;
+    }
+
+    onMounted(() =>{
+        profile.value = {
+            bio: user.value.bio,
+            display_name: user.value.display_name,
+            career: user.value.career, 
+        }
+    })
+
+    return {
+        profile,
+        updating,
+        feedback,
+        handleSubmit,
+
+    }
+
+}
+
 
 // Variable para guardar la función de cancelar la suscripción a la autenticación.
-let unsubAuth = () => {}
+/* let unsubAuth = () => {}
 
 export default {
     name: 'MyProfileEdit',
@@ -72,43 +137,31 @@ export default {
         // Cancelamos la suscripción.
         unsubAuth();
     }
-}
+} */
 </script>
 
 <template>
     <MainH1>Editar mi perfil</MainH1>
 
-    <div
-        v-if="feedback.message != null"
-        class="p-4 mb-4 rounded"
-        :class="{
-            'bg-red-100 text-red-700': feedback.type === 'error',
-            'bg-green-100 text-green-700': feedback.type === 'success',
-        }"
-    >
+    <div v-if="feedback.message != null" class="p-4 mb-4 rounded" :class="{
+        'bg-red-100 text-red-700': feedback.type === 'error',
+        'bg-green-100 text-green-700': feedback.type === 'success',
+    }">
         {{ feedback.message }}
     </div>
 
-    <form 
-        action="#"
-        @submit.prevent="handleSubmit"
-    >
+    <form action="#" @submit.prevent="handleSubmit">
         <div class="mb-4">
             <label for="bio" class="block mb-2 text-white">Biografía</label>
-            <textarea
-                id="bio"
+            <textarea id="bio"
                 class="w-full p-2 rounded-xl border border-white/30 bg-white/10 backdrop-blur-md shadow-lg "
-                v-model="profile.bio"
-            ></textarea>
+                v-model="profile.bio"></textarea>
         </div>
         <div class="mb-4">
             <label for="display_name" class="block mb-2 text-white">Nombre de usuario</label>
-            <input
-                type="text"
-                id="display_name"
+            <input type="text" id="display_name"
                 class="w-full p-2 rounded-xl border border-white/30 bg-white/10 backdrop-blur-md shadow-lg"
-                v-model="profile.display_name"
-            >
+                v-model="profile.display_name">
         </div>
         <!-- <div class="mb-4">
             <label for="career" class="block mb-2">Carrera</label>
@@ -122,7 +175,7 @@ export default {
         <MainButton type="submit">
             <!-- {{ !updating ? 'Actualizar' : 'Cargando...' }} -->
             <template v-if="!updating">Actualizar</template>
-            <MainLoader v-else /> 
+            <MainLoader v-else />
         </MainButton>
     </form>
 </template>
