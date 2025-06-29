51ca4ce98ffb2c56d04c0d1a95c2066339505ccd
Julian Toledo <84879981+juliantoledo94@users.noreply.github.com>
2025-06-29 11:10:40 -0300

    Update MyProfileEdit

--- src/pages/MyProfileEdit.vue
+++ src/pages/MyProfileEdit.vue
@@ -69,75 +69,6 @@ function useProfileEditForm(user) {
 }
 
 
-// Variable para guardar la función de cancelar la suscripción a la autenticación.
-/* let unsubAuth = () => {}
-
-export default {
-    name: 'MyProfileEdit',
-    components: { MainH1, MainLoader, MainButton },
-    data() {
-        return {
-            user: {
-                id: null,
-                email: null,
-                display_name: null,
-                bio: null,
-                career: null,
-            },
-            profile: {
-                display_name: null,
-                bio: null,
-                career: null,
-            },
-            updating: false,
-
-            feedback: {
-                type: 'success',
-                message: null,
-            }
-        }
-    },
-    methods: {
-        async handleSubmit() {
-            // Limpiamos el mensaje de feedback.
-            this.feedback.message = null;
-
-            try {
-                this.updating = true;
-                await updateAuthUserProfile({
-                    ...this.profile
-                });
-                this.updating = false;
-
-                this.feedback = {
-                    type: 'success',
-                    message: 'Tu perfil se actualizó con éxito.',
-                }
-            } catch (error) {
-                this.feedback = {
-                    type: 'error',
-                    message: 'Ocurrió un error al actualizar el perfil.',
-                }
-            }
-        }
-    },
-    mounted() {
-        unsubAuth = subscribeToUserState(newUserState => {
-            this.user = newUserState;
-
-            // Cargamos los datos iniciales del form.
-            this.profile = {
-                bio: this.user.bio,
-                display_name: this.user.display_name,
-                career: this.user.career,
-            }
-        });
-    },
-    unmounted() {
-        // Cancelamos la suscripción.
-        unsubAuth();
-    }
-} */
 </script>
 
 <template>
@@ -163,17 +94,9 @@ export default {
                 class="w-full p-2 rounded-xl border border-white/30 bg-white/10 backdrop-blur-md shadow-lg"
                 v-model="profile.display_name">
         </div>
-        <!-- <div class="mb-4">
-            <label for="career" class="block mb-2">Carrera</label>
-            <input
-                type="text"
-                id="career"
-                class="w-full p-2 border border-gray-400 rounded"
-                v-model="profile.career"
-            >
-        </div> -->
+     
         <MainButton type="submit">
-            <!-- {{ !updating ? 'Actualizar' : 'Cargando...' }} -->
+            
             <template v-if="!updating">Actualizar</template>
             <MainLoader v-else />
         </MainButton>
